45a7c523e6937393d3cb73fd96253b0fcb43f677
JeanMarc <jeanmarc.rajaonarivelona01@gmail.com>
2023-11-19 14:51:33 +0100

    feat: implements CrudOperations in SubscribersCrudOperations

---
+++ src/main/java/Interface/SubscribersCrudOperations.java
@@ -0,0 +1,25 @@
+package Interface;
+
+import java.util.List;
+
+public class SubscribersCrudOperations  implements CrudOperations{
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public List saveAll(List toSave) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object toSave) {
+        return null;
+    }
+
+    @Override
+    public Object delete(Object toDelete) {
+        return null;
+    }
+}
