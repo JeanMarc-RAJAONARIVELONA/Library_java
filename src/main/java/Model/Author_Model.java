89c64a9e6dbaf2b9ef0218d51cc8405998c03104
JeanMarc <jeanmarc.rajaonarivelona01@gmail.com>
2023-11-19 13:48:32 +0100

    feat: create Author_Model, Book_Model and Subscribers_Model

---
+++ src/main/java/Model/Author_Model.java
@@ -0,0 +1,13 @@
+package Model;
+
+import lombok.*;
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Author_Model {
+    private int id_author;
+    private String author_name;
+    private String author_sex;
+}
