538ecb403b488d7c74e0963258bf653dd5cf8ef0
JeanMarc <jeanmarc.rajaonarivelona01@gmail.com>
2023-11-19 14:13:47 +0100

    fix: add references to script

--- Script Content/Subscribers_script.sql
+++ Script Content/Subscribers_script.sql
@@ -1,7 +1,8 @@
 create table if not exists subscribers(
   id_subscribers serial primary key,
   subscribers_name varchar(250) not null,
-  subscribers_email varchar(150)
+  subscribers_email varchar(150),
+  references varchar(250)
 );
 
 insert into subscribers (subscribers_name, subscribers_email) values ('Tilly Banaszkiewicz', 'tbanaszkiewicz0@artisteer.com'),('Hurleigh Harland', 'hharland1@soundcloud.com'),('Arda Pleager', 'apleager2@vkontakte.ru');
