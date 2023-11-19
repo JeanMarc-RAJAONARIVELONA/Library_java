c85daeb558eb78006f278f0f186b88b1ff134779
JeanMarc <jeanmarc.rajaonarivelona01@gmail.com>
2023-11-19 15:30:11 +0100

    fix: resolve some mistake into BookCrudOperations

--- src/main/java/Interface/BookCrudOperations.java
+++ src/main/java/Interface/BookCrudOperations.java
@@ -1,26 +1,28 @@
 package Interface;
 
+import Model.Book_Model;
+
 import java.util.List;
 
-public class BookCrudOperations implements CrudOperations{
+public class BookCrudOperations implements CrudOperations<Book_Model>{
 
     @Override
-    public List findAll() {
+    public List<Book_Model> findAll() {
         return null;
     }
 
     @Override
-    public List saveAll(List toSave) {
+    public List<Book_Model>saveAll(List toSave) {
         return null;
     }
 
     @Override
-    public Object save(Object toSave) {
+    public Book_Model save(Book_Model toSave) {
         return null;
     }
 
     @Override
-    public Object delete(Object toDelete) {
+    public Book_Model delete(Book_Model toDelete) {
         return null;
     }
 }
